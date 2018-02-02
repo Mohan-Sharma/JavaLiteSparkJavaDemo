81ae96cb0ede623a8c5166776d3411a421366a8b
Mohan Sharma <mtarrnesharma@gmail.com>
2018-02-02 15:09:24 +0530

    updated script

--- basic.sql
+++ basic.sql
@@ -1,8 +1,6 @@
 CREATE TABLE `student` (
-  `roll_number` int(11) NOT NULL AUTO_INCREMENT,
+  `roll_number` int(11) NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`roll_number`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-insert into student(roll_number, first_name, last_name) value(1, "Firstname", "Lastname");
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
